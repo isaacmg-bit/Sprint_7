6188bca6e96ca98ef2a24995c0577bc128f52144
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-01-31 15:20:18 +0100

    feat: made the home screen and the navbar buttons more responsive for small screens

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
