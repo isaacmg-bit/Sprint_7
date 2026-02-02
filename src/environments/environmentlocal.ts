3475c5f4f03cdc3aff3aa5d870c7240002f7d82c
isaacmg-bit <isaacmalagongarcia@gmail.com>
2026-02-02 22:31:32 +0100

    Feature/movie card (#2)
    * fix: added environment file to gitignore
    
    * fix: removed sensible data from environment

---
+++ src/environments/environmentlocal.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  apiToken: '',
+  apiUrl: 'https://api.themoviedb.org/3/',
+  firebaseConfig: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+  },
+};
